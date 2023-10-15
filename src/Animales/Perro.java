b4087b4d915d1f27e842b685902dd962153e97e6
carol <carol@Carolina>
2023-10-15 15:48:39 -0500

    Ejercicio 14

---
+++ src/Animales/Perro.java
@@ -0,0 +1,20 @@
+package Animales;
+
+public class Perro extends Cánido {
+
+    public String getSonido() {
+        return "Ladrido";
+    }
+
+    public String getAlimentos() {
+        return "Carnivoro";
+    }
+
+    public String getHábitat() {
+        return "Domestico";
+    }
+
+    public String getNombreCientífico() {
+        return "Canis lupus familiaris";
+    }
+}
